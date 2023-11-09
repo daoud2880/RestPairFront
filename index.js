07b9e7058826d5dc89953f21a05a24db1220f9a9
daoudahmad <daoud@hotmail.dk>
2023-11-09 14:12:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const baseUri = "https://restpairdaoudluna.azurewebsites.net/api/MusicRecords"
+
+Vue.createApp({
+    data() {
+        return {
+            musicRecords: [],
+            name: null,
+            message: null,
+            title:null,
+            artist:null,
+            release:null,
+            genre:null,
+
+        }
+    },
+    methods: {
+        sayHello(name) {
+            if (name)
+                this.message = "Hello " + name
+            else
+                this.message = "Hello No Name"
+        },
+        getAll(){
+            this.helperGetAndShow(baseUri)
+        },
+        async helperGetAndShow(uri) {
+            try {
+                const response = await axios.get(uri)
+                this.musicRecords = await response.data
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+
+    }
+}).mount("#app")
+
